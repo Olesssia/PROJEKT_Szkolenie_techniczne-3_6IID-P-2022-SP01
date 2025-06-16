59d6ca1da2abd97735792371d4465801c648e9ed
w68162 <w68162@student.wsiz.edu.pl>
2025-06-16 12:33:44 +0200

    ReaderService is added.

---
+++ ReaderService/Data/ReaderDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReaderService.Models;
+using System.Collections.Generic;
+
+namespace ReaderService.Data
+{
+    public class ReaderDbContext : DbContext
+    {
+        public ReaderDbContext(DbContextOptions<ReaderDbContext> options) : base(options) { }
+
+    public DbSet<Reader> Readers => Set<Reader>();
+}
+}
